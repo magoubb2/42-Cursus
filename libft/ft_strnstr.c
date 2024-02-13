79844f239d298fd241bfbc481375100e309c157d
Marguerite Baron Beliveau <marbaron@c1r8p7.42quebec.com>
2024-02-13 11:43:03 -0500

    addded comment

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -13,6 +13,17 @@
 
 #include "libft.h"
 
+// 1). Check for empty (needle) str and (len) is 0, if true return (haystack).
+// 2). Check if both (needle) and (haystack) are empty, if true return
+//     (haystack).
+// 3). While (haystack) and (len) is not == to 0,
+//          1). Check if (len) is then len of (needle), if true, return
+//              NULL.
+//          2). Cast (haystack -> h) and (needle -> n) to const char *.
+//          3). while (*h) == (*n) and (*h != '\0') and (*n != '\0) and 
+//              len != 0 if true, we increment (h) and (n).
+//          4). Check if (*n) == '\0' if true, return haystack.
+//          5). Increment (haystack) and decrement (len).
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
     const char *n;
@@ -22,13 +33,13 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t len)
         return ((char *)haystack);
     if (haystack[0] == '\0' && needle[0] == '\0')
         return ((char *)haystack);
-    while (*haystack && len)
+    while (*haystack && len != 0)
     {
         if (len < ft_strlen(needle))
             return (NULL);
         h = haystack;
         n = needle;
-        while (*h == *n && *n && *h && len)
+        while (*h == *n && *n && *h && len != 0)
         {
             h++;
             n++;
