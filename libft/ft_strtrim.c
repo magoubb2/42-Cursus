4f58005041a2d8ff3b1d4354b0ff546a79855401
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2024-02-16 14:07:29 -0500

    ft_strtrim(done + need comments)

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -1,36 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: margueritebaronbeliveau <margueritebaro    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 15:26:05 by margueriteb       #+#    #+#             */
+/*   Updated: 2024/02/16 14:05:37 by margueriteb      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
 char *ft_strtrim(char const *s1, char const *set)
 {
 	int i;
-	int j;
+	int start;
+	int end;
 	int str_len;
 	char *new_str;
 
-	i = 0;
-	j = 0;
-	str_len = ft_strlen(s1) - (ft_strlen(set) * 2);
-	new_str = (char *)malloc(sizeof(char *) * (str_len));
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	new_str = (char *)malloc(sizeof(char) * (end - start) + 2);
 	if (!new_str)
 		return (NULL);
-	while (s1[i] == set[i])
-	{
-		i++;
-		j++;
-	}
 	i = 0;
-	while (s1[i])
-	{
-		new_str[i] = s1[j];
-		i++;
-		j++;
-	}
+	while (start <= end)
+		new_str[i++] = s1[start++];
 	new_str[i] = '\0';
 	return (new_str);
 }
 
+
 // int main(void)
 // {
-// 	printf("%s\n", ft_strtrim("abc hello  world abc  lol", "abc"));
+// 	printf("%s\n", ft_strtrim("abc hello  world abc lol abc", "abc"));
 // }
