9e001d1725958aaf7aef4bd3af0b470fb4a6f66c
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2024-02-18 17:58:35 -0500

    ft_itoa(done + need comments)

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -3,38 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marbaron <marbaron@student.42.fr>          +#+  +:+       +#+        */
+/*   By: margueritebaronbeliveau <margueritebaro    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 17:14:08 by marbaron          #+#    #+#             */
-/*   Updated: 2024/02/18 15:46:57 by marbaron         ###   ########.fr       */
+/*   Updated: 2024/02/18 17:56:57 by margueriteb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_itoa(int n)
+static int len_n(int nb)
 {
-	int i;
-	int nb;
 	int len;
-	char *str;
-
-	nb = n;
+	
+	len = 0;
+	if (nb <= 0)
+		len++;
 	while (nb != 0)
 	{
 		nb = nb / 10;
 		len++;
 	}
+	return (len);
+}
+
+char *ft_itoa(int n)
+{
+	int len;
+	char *str;
+
+	len = len_n(n);
 	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len--] = '\0';
+	if (n == 0)
+	{
+		str[0] = '0';
+		return (str);
+	}
+	if (n < 0)
+		str[0] = '-';
 	while (n != 0)
 	{
-		str[len--] = '0' + n % 10;
+		if (str[0] == '-' && n < 0)
+			str[len--] = '0' - ((n % 10));
+		else
+		 	str[len--] = '0' + (n % 10);
 		n = n / 10;
 	}
 	return (str);
 }
 
-int main(void)
-{
-	printf("%s\n", ft_itoa(12345));
-}
+// int main(void)
+// {
+// 	printf("%s\n", ft_itoa(-1));
+// }
