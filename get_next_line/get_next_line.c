917840d0f2a79f1c56f21875cc3dd299b78c10a6
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2024-02-21 14:10:56 -0500

    push gnl

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,8 +6,41 @@
 /*   By: margueritebaronbeliveau <margueritebaro    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 10:59:54 by margueriteb       #+#    #+#             */
-/*   Updated: 2024/02/21 11:02:03 by margueriteb      ###   ########.fr       */
+/*   Updated: 2024/02/21 14:10:24 by margueriteb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/* Function to clean up. */
+
+// 1). clean up buffer after each line is stash.
+
+/* function to extract each line. */
+
+// 1). Extract line from the buffer.
+
+/* function that read. */
+
+char *read_from_fd(int fd, char *str)
+{
+    // 1). Initialise a buffer to store the character read from the fd.
+    // 2). Initialise counter to keep track of the current pos in the buffer.
+    char    *buffer;
+    int     byte;
+    // 3). use the read function.
+    return (str);
+}
+
+/* The get_next_line function. */
+char *get_next_line(int fd)
+{
+    char *buffer;
+    char *line;
+
+    /* function that read. */
+    buffer = read_from_fd(fd, buffer);
+    /* function to extract each line. */
+    /* Function to clean up. */
+    return (line);
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,16 @@
 /*   By: margueritebaronbeliveau <margueritebaro    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 10:59:56 by margueriteb       #+#    #+#             */
-/*   Updated: 2024/02/21 11:01:05 by margueriteb      ###   ########.fr       */
+/*   Updated: 2024/02/21 13:58:43 by margueriteb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+# include <unistd.h>
 
+#define BUFFER_SIZE 1024
 
 #endif
