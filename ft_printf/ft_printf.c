b719e5bd4134c8b2ad90928895c0d8adc3c57df6
Marguerite Baron Beliveau <marbaron@c1r8p5.42quebec.com>
2024-03-09 15:55:19 -0500

    FT_PRINTF(big fix in francinette, was forgeting to return in convert)

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: marbaron <marbaron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 11:09:22 by marbaron          #+#    #+#             */
-/*   Updated: 2024/03/08 15:47:26 by marbaron         ###   ########.fr       */
+/*   Updated: 2024/03/09 15:51:51 by marbaron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,30 @@ static int convert(va_list args, const char place_holder)
 		// Convert the place holder for a char.
 		return (ft_putchar_fd_pf(va_arg(args, int), 1));
 	// %s -> ft_putstr_fd
-	if (place_holder == 's')
+	else if (place_holder == 's')
 		// Convert the place holder for a string.
-		ft_putstr_fd_pf(va_arg(args, char *), 1);
+		return (ft_putstr_fd_pf(va_arg(args, char *), 1));
 	// %i -> ft_putnbr_fd || %d -> ft_putnbr_fd
-	if (place_holder == 'i' || place_holder == 'd')
+	else if (place_holder == 'i' || place_holder == 'd')
 		// Convert the place holder for a int.
-		ft_putnbr_fd_pf(va_arg(args, int), 1);
+		return (ft_putnbr_fd_pf(va_arg(args, int), 1));
 	// %u -> ft_undecimal_fd
-	if (place_holder == 'u')
+	else if (place_holder == 'u')
 		// Convert the place holder for a unsigned int.
-		ft_unsigned_fd_pf(va_arg(args, unsigned int), 1);
+		return (ft_unsigned_fd_pf(va_arg(args, unsigned int), 1));
 	// %x -> ft_puthex_fd
-	if (place_holder == 'x')
+	else if (place_holder == 'x')
 		// Convert the place holder for a integer as an hexadecimal in lower case.
-		ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef");
+		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789abcdef"));
 	// %X -> ft_putheX_fd
-	if (place_holder == 'X')
+	else if (place_holder == 'X')
 		// Convert the place holder for a integer as an hexadecimal in upper case.
-		ft_putnbr_base(va_arg(args, unsigned int), "0123456789ABCDEF");
+		return (ft_putnbr_base(va_arg(args, unsigned int), "0123456789ABCDEF"));
 	// %p -> ft_putpointer_fd
 	// if (place_holder == 'p')
 		// Convert the place holder for a pointer's address
-	if (place_holder == '%')
-		ft_putchar_fd_pf('%', 1);
+	else if (place_holder == '%')
+		return (ft_putchar_fd_pf('%', 1));
 	return (0);
 }
 
@@ -70,32 +70,35 @@ int ft_printf(const char *format, ...)
 			print = print + ft_putchar_fd_pf(format[i], 1);
 		i++;
 	}
+	// printf("i: %i\n", i);
+	// printf("print: %i\n", print);
+	
 	va_end(args);
 	return (print);
 }
 
 // int main(void)
 // {
-// 	char *str = "hello";
-// 	char c = 'c';
-// 	int i = 34;
-// 	int ui = -34;
-// 	int x = 11;
+	// char *str = "hello";
+	// char c = 'c';
+	// int i = 34;
+	// int ui = -34;
+	// int x = 11;
 	
-// 	ft_printf("string: [%s]\n", str);
-// 	ft_printf("\n");
-// 	ft_printf("char: [%c]\n", c);
-// 	ft_printf("\n");
-// 	ft_printf("int: [%i]\n", i);
-// 	ft_printf("\n");
-// 	ft_printf("mine unsigned int: [%u]\n", ui);
-// 	ft_printf("\n");
-// 	printf("real unsigned int: [%u]\n", ui);
-// 	ft_printf("\n");
-// 	ft_printf("hexadecimal x: [%x]\n", x);
-// 	ft_printf("hexadecimal X: [%X]\n", x);
-// 	ft_printf("\n");
-// 	ft_printf("mine test: %%");
-// 	printf("real test: %%");
-// 	ft_printf("\n");
+	// ft_printf("test\n");
+	// ft_printf(" [%s] ", str);
+	// ft_printf("string: [%s]\n", str);
+	// ft_printf("\n");
+	// ft_printf("char: [%c]\n", c);
+	// ft_printf("\n");
+	// ft_printf("int: [%i]\n", i);
+	// ft_printf("\n");
+	// ft_printf("mine unsigned int: [%u]\n", ui);
+	// ft_printf("\n");
+	// ft_printf("\n");
+	// ft_printf("hexadecimal x: [%x]\n", x);
+	// ft_printf("hexadecimal X: [%X]\n", x);
+	// ft_printf("\n");
+	// ft_printf("mine test: %%\n");
+	// printf("real test: %%\n");
 // }
