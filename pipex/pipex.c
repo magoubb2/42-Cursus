7cb3508d0c4ab1c39500c9681839db6e0482c7d2
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2024-03-23 17:56:15 -0400

    PIPEX(code structure)

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,11 +6,12 @@
 /*   By: margueritebaronbeliveau <margueritebaro    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 11:02:01 by margueriteb       #+#    #+#             */
-/*   Updated: 2024/03/22 14:28:48 by margueriteb      ###   ########.fr       */
+/*   Updated: 2024/03/23 17:55:21 by margueriteb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
 
 /* The command we have to execute : < file1 cmd1 | cmd2 > file2 */
 
@@ -26,3 +27,32 @@
 
 /* Wait for all the process to end before writing to the outfile. */
 
+
+int main(int argc, char **argv, char **env) 
+{
+    if (argc != 5)
+        // return error
+
+    // 1). Check for errors.
+    
+    // 2). Initialise infile.
+    
+    // 3). Initialise outfile.
+
+    // 4). Create necessary pipe.
+
+    // 5). Get the path.
+
+    // 6).Get the command path.
+
+    // 7). First child process.
+        // Execute the first command.
+    
+    // 8).Second child process.
+        // Execute second command.
+    
+    // 9).
+    // Close pipe.
+    // Wait for first child.
+    // Wait for second child.
+}
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: margueritebaronbeliveau <margueritebaro    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 11:02:06 by margueriteb       #+#    #+#             */
-/*   Updated: 2024/03/22 11:08:41 by margueriteb      ###   ########.fr       */
+/*   Updated: 2024/03/23 17:49:21 by margueriteb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,18 @@
 
 #include <unistd.h>
 #include <sys/wait.h>
+
+typedef struct s_data
+{
+    int fd[2];
+    int infile;
+    int outfile;
+    pid_t pid_cmd1;
+    pid_t pid_cmd2;
+    char *path;
+    char **path_to_cmd;
+    char **cmd_args;
+    char *cmd;
+}   t_data;
 
 #endif
