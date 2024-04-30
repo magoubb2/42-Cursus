76344357a7966735ebb0fbdb221b91d719bab6c7
mabaron- <margueritebaronbeliveau@marguereslaptop.home>
2024-04-30 12:43:10 -0400

    struct defined

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marbaron <marbaron@student.42.fr>          +#+  +:+       +#+        */
+/*   By: margueritebaronbeliveau <margueritebaro    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 11:30:25 by marbaron          #+#    #+#             */
-/*   Updated: 2024/04/29 11:32:07 by marbaron         ###   ########.fr       */
+/*   Updated: 2024/04/30 12:42:36 by margueriteb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 # define SO_LONG_H
 
 # include "mlx/mlx.h"
+
+typedef struct s_data
+{
+    
+}   t_data;
 
 #endif
