b9c95e827c5f4ec581c469f7dd7ba6e98efd1267
mabaron- <margueritebaronbeliveau@marguerites-Laptop.local>
2024-04-30 10:16:17 -0400

    map functions

--- so_long/src/map.c
+++ so_long/src/map.c
@@ -3,12 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marbaron <marbaron@student.42.fr>          +#+  +:+       +#+        */
+/*   By: margueritebaronbeliveau <margueritebaro    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 11:42:23 by marbaron          #+#    #+#             */
-/*   Updated: 2024/04/29 11:43:29 by marbaron         ###   ########.fr       */
+/*   Updated: 2024/04/30 10:15:49 by margueriteb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void line_len_validity(char *map)
+{
+    // Get the length of the first line and compare it to all of the 
+    // following line.
+}
+
+void nb_character_validity(char *map, t_data *data)
+{
+    // If the number of 'P' is not 1, if the number of 'E' is not 1 and
+    // if the number of C is less than 1 return an error msg.
+}
+
+void character_validity(char *map)
+{
+    // If the characters in the map are not: 1, 0, P, E, C or \n
+    // return an error msg.
+}
+
+void side_wall_validity(t_data *data)
+{
+    // Check each of the side wall of the map and if they are not
+    // 1 return an error msg.
+}
+
+void map_validity(char *map, t_data *data)
+{
+    // Function that checks for the valid character in the map.
+    // Function that checks for the valid number of character.
+    // Function that check if every line of the map is the same length.
+}
